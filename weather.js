9df2080489b4fa7e54d8e4b0a5a1eef21b49a5e7
yanghyunjun <akakak231@gmail.com>
2019-08-26 22:03:48 +0900

    Create ToDoList Use to VanillaJS

--- weather.js
+++ weather.js
@@ -1,5 +1,37 @@
-function init{
+const COORDS = "coords";
 
+function saveCoords(GeolocationObj) {
+  localStorage.setItem(COORDS, JSON.stringify(GeolocationObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const GeolocationObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(GeolocationObj);
+}
+
+function handleGeoError() {
+  console.log("cant access geolocation");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
 }
 
 init();
